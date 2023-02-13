ecf6cdd5ae830d0e9d2e6e66fce9cee96a1e8951
Joel Frydman <joelfrydman@hotmail.com>
2023-02-13 18:22:11 -0300

    se incorporo weather api

--- js/main.js
+++ js/main.js
@@ -322,3 +322,50 @@ function actualizarNumero() {
     numero.innerText= nuevoNumero;
     
 }
+
+let lon;
+let lat;
+let temperature = document.querySelector(".temp")
+let summary = document.querySelector(".summary")
+let loc = document.querySelector(".location")
+const kelvin =273.15;
+
+
+window.addEventListener("load",()  => {
+
+if (navigator.geolocation) {
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+         lon = position.coords.longitude;
+         lat = position.coords.latitude;
+
+
+         const api_id= "df627beeb16e2d35c74e2ad2c29e80d1";
+         const url_base= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&`+`lon=${lon}&appid=${api_id}`;
+
+
+         fetch(url_base)
+         .then((response) =>{
+         return response.json();
+
+        
+        })
+
+        .then((data)=>{ 
+         console.log(data);
+
+        temperature.textContent= Math.floor(data.main.temp - kelvin) + "°C";
+        summary.textContent=data.weather[0].description;
+        loc.textContent=data.name + "," + data.sys.country;
+
+        });
+
+
+    });
+
+    
+}
+
+
+});
+
